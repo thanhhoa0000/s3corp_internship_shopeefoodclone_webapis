b3d56dae1427e1413d01c4c11ecef6885f405cbb
thanhhoa0000 <hoathanhnguyen.work@gmail.com>
2025-05-09 14:56:21 +0700

    test: Do UnitTesting for Login, Register and CreateStore with NUnit

---
+++ Tests/Identity/Identity.UnitTest/GlobalUsings.cs
@@ -0,0 +1,17 @@
+﻿global using System.Linq.Expressions;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.Extensions.Configuration;
+
+global using Moq;
+global using AutoMapper;
+global using FluentAssertions;
+global using NUnit.Framework;
+
+global using ShopeeFoodClone.WebApi.Identity.Domain.Interfaces;
+global using ShopeeFoodClone.WebApi.Identity.Domain.Entities;
+global using ShopeeFoodClone.WebApi.Identity.Domain.Enums;
+    
+global using ShopeeFoodClone.WebApi.Identity.Application.Interfaces;
+global using ShopeeFoodClone.WebApi.Identity.Application.Models.Dtos;
+global using ShopeeFoodClone.WebApi.Identity.Application.Services;
